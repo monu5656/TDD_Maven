163abf6f04b01d8c710952703d25ae9e03db7803
IBM <IBM@DEVOPS_02>
2018-07-31 09:05:35 +0530

    donwe

--- Testing/src/test/java/testing/Testing.java
+++ Testing/src/test/java/testing/Testing.java
@@ -22,7 +22,7 @@ public class Testing {
 	  }
 	  @BeforeClass
 	  public void beforeClass() {
-		  System.setProperty("webdriver.gecko.driver", "E:\\geckodriver-v0.20.1-win64\\geckodriver.exe");  //"/usr/local/bin/geckodriver"
+		  System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");  //"E:\\geckodriver-v0.20.1-win64\\geckodriver.exe"
 		  driver= new FirefoxDriver();
 		  driver.get("http://ec2-18-222-145-43.us-east-2.compute.amazonaws.com:8090/TestWebapp");
 		  
