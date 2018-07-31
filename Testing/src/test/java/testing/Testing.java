a2d9801afe81e2f59e81d79d5ffae2b52c2c056e
IBM <IBM@DEVOPS_02>
2018-07-31 13:34:52 +0530

    com

--- Testing/src/test/java/testing/Testing.java
+++ Testing/src/test/java/testing/Testing.java
@@ -25,7 +25,7 @@ public class Testing {
 		  System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
 		  //System.setProperty("webdriver.gecko.driver", "E:\\geckodriver-v0.20.1-win64\\geckodriver.exe");
 		  driver= new FirefoxDriver();
-		  driver.get("http://ec2-18-221-62-120.us-east-2.compute.amazonaws.com:8090/TestWebapp");
+		  driver.get("http://ec2-18-188-48-97.us-east-2.compute.amazonaws.com:8090/TestWebapp");
 		  
 	  }
 
