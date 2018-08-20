c9f0d5a427b04266f9eac6fbc33c7ef2e478af5e
monu68669 <monuburnwal786@gmail.com>
2018-08-20 20:54:18 +0530

    Update Testing.java

--- Testing/src/test/java/testing/Testing.java
+++ Testing/src/test/java/testing/Testing.java
@@ -12,20 +12,15 @@ public class Testing {
 	WebDriver driver;
 	  @Test
 	  public void login() {
-		  driver.findElement(By.name("uname")).sendKeys("admin");
-		  driver.findElement(By.name("psw")).sendKeys("pass");
-		  driver.findElement(By.xpath("//*[@type='submit']")).click();
-		  System.out.println("Sucessful");
-		  String breakpoint=driver.findElement(By.tagName("h2")).getText();
-		  Assert.assertEquals(breakpoint, "Prolearn");
+		  driver.findElement(By.name("userName")).sendKeys("sunil");
+		  driver.findElement(By.name("password")).sendKeys("sunil");
 		  
 	  }
 	  @BeforeClass
 	  public void beforeClass() {
 		  System.setProperty("webdriver.gecko.driver", "E:\\Gecofriver for BDD Firefox\\geckodriver.exe");
-		  //System.setProperty("webdriver.gecko.driver", "E:\\geckodriver-v0.20.1-win64\\geckodriver.exe");
 		  driver= new FirefoxDriver();
-		  driver.get("localhost:8090/TestWebapp");
+		  driver.get("http://newtours.demoaut.com/");
 		  
 	  }
 
